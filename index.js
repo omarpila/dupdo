2db1fada74a414d4657c458b54b1d3ffcd004741
OmarPila <omarpila05@gmail.com>
2022-05-24 22:45:51 -0500

    parte 2

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const TOKEN = "OTc2Njc5ODAwMDQ5NjY0MDYw.GDDoGG.5km_QwAP15MWyNWQDPnBDtOhA0DbSk4rWszsLc"
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -16,7 +17,18 @@ client.on("ready", () => {
 
 client.on("messageCreate", (message) => {
     if (message.content == "hi"){
-      message.reply("Hello World!")
+        message.reply("Hello World!")
     }
 })
+
+const welcomeChannelId = "947745064891854858"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+})
+
 client.login(process.env.TOKEN)
